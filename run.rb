758d7e0c9dae518e72b46350aae03dec663fbc6a
Jonas Nielsen <Thecichos@gmail.com>
2017-08-08 12:52:38 +0200

    made a more modular json pattern and made a better reload function for the lists

--- run.rb
+++ run.rb
@@ -1,38 +1,58 @@
 require "rubygems"
 require "json"
 require "green_shoes"
+require "unicode"
+
 
 Shoes.app width: 600 do
+
   file = File.read("json/cars.json")
-  fileParsed = JSON.parse(file)
+
+  def loadJson file
+    return JSON.parse(file)
+  end
 
   make_list = []
   model_list = []
-  interval_value = 0
-  while interval_value != fileParsed.length
-    if make_list.include? fileParsed[interval_value]["make"]
 
-    else
-      make_list.push(fileParsed[interval_value]["make"])
+  fileParsed = loadJson(file)
+
+
+stack do
+  def generate_make_list fileParsed, make_list
+    # list the makes of the cars
+    interval_value = 0
+    while interval_value != fileParsed.length
+      if make_list.include? fileParsed[interval_value]["make"]
+      else
+        make_list.push(fileParsed[interval_value]["make"])
+      end
+      interval_value = interval_value + 1
     end
-    interval_value = interval_value + 1
+    return make_list
   end
+  make_list = generate_make_list fileParsed, make_list
   choice1 = ""
   choice2 = ""
-  @make_list_choice = para
+  @make_list_choice = para "", margin: 10
 
   flow margin: 10 do
     @make_list = list_box items: make_list do |e|
       choice1 = e.text
       @make_list_choice.text = "#{choice1} #{choice2}"
       interval_value = 0
+
+      # list the models of the cars
+
+      model_list = []
       while interval_value != fileParsed.length
         if fileParsed[interval_value]["make"] == e.text
-          @model_list.items = fileParsed[interval_value]["models"]
-        end
+          model_list.push(fileParsed[interval_value]["model"])
 
+        end
         interval_value = interval_value + 1
       end
+      @model_list.items = model_list
       re = @model_list.items
       @model_list.choose re[0]
     end
@@ -47,4 +67,63 @@ Shoes.app width: 600 do
   @make_list.choose re[0]
   re = @model_list.items
   @model_list.choose re[0]
+end
+stack margin: 10 do
+  @error = para
+  flow do
+    para "please write a car manufacurer", margin: 5
+    @add_make = edit_line " ", margin: 5
+  end
+  flow do
+    para "please write a car model", margin: 5
+    @add_model = edit_line " ", margin: 5
+  end
+  @confirm = button "confirm" do
+    if @add_make.text.to_s.empty?
+        @error.replace("you need to fill the make line")
+        timer 5 do
+          @error.replace(" ")
+        end
+    elsif @add_model.text.to_s.empty?
+        @error.replace("you need to fill the model line")
+        timer 5 do
+          @error.replace(" ")
+        end
+    else
+      interval_value = 0
+      while interval_value != fileParsed.length
+          if fileParsed[interval_value]["model"] == @add_model.text.capitalize
+            @error.replace("this is already in the library")
+            break
+          end
+          temp = {"make" => @add_make.text.capitalize,"model" => @add_model.text.capitalize}
+          fileParsed << temp
+          File.open("json/cars.json", "w") do |f|
+            f.puts JSON.pretty_generate(fileParsed)
+            f.close
+
+          end
+
+          file = File.read("json/cars.json")
+
+          fileParsed = loadJson(file)
+
+          make_list = generate_make_list(fileParsed, make_list)
+
+          @make_list.items = []
+          @make_list.items = make_list
+
+          re = @make_list.items
+          @make_list.choose re[0]
+
+          puts @make_list.items
+
+          break
+        interval_value = interval_value + 1
+      end
+
+      end
+    end
+  end
+
 end
