d5a8d35980fc51393ed105509db437c4c2b68269
thecichos <thecichos@gmail.com>
2017-08-10 12:38:03 +0200

    more commit

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-$getHash = $_GET["getHash"];
-if ($getHash == true) {
-  exit(md5_file("json/cars.json"));
+if ($_GET) {
+  $getHash = $_GET["getHash"];
+  if ($getHash == true) {
+    exit(md5_file("json/cars.json"));
+  }
 }
 ?>
 <!DOCTYPE html>
@@ -16,35 +18,37 @@ if ($getHash == true) {
     <title>cars list</title>
   </head>
   <body>
-    <table>
-      <tr>
-        <td><b>make</b></td>
-        <td><b>model</b></td>
-        <td><b>engine size</b></td>
-        <td><b>action</b></td>
-      </tr>
-    <?php
-      $file = file_get_contents("json/cars.json");
+    <div id="container">
+      <table>
+        <tr>
+          <td><b>make</b></td>
+          <td><b>model</b></td>
+          <td><b>engine size</b></td>
+          <td><b>action</b></td>
+        </tr>
+      <?php
+        $file = file_get_contents("json/cars.json");
 
-      $fileParsed = jsonLoad::__loadAndParse($file);
-      for ($i=0; $i < count($fileParsed) ; $i++) {
-        print_r("<tr>");
-        print_r("<td><input type='text' value=".$fileParsed[$i]['make']." disabled></td>");
-        print_r("<td><input type='text' value=".$fileParsed[$i]['model']." disabled></td>");
-        if (isset($fileParsed[$i]["engine"]) == false) {
-          print_r("<td><input type='text' value='not specified' disabled></td>");
-        } else {
-          print_r("<td><input type='text' value=".$fileParsed[$i]['engine']." disabled></td>");
+        $fileParsed = jsonLoad::__loadAndParse($file);
+        for ($i=0; $i < count($fileParsed) ; $i++) {
+          print_r("<tr>");
+          print_r("<td><input type='text' value=".$fileParsed[$i]['make']." disabled></td>");
+          print_r("<td><input type='text' value=".$fileParsed[$i]['model']." disabled></td>");
+          if (isset($fileParsed[$i]["engine"]) == false) {
+            print_r("<td><input type='text' value='not specified' disabled></td>");
+          } else {
+            print_r("<td><input type='text' value=".$fileParsed[$i]['engine']." disabled></td>");
+          }
+          print_r("</tr>");
         }
-        print_r("</tr>");
-      }
-     ?>
-     <script>
-      $(document).ready(function() {
-        $("table").append("<tr><td><input type=text value='please enter make of car'></td><td><input type=text value='please enter model of car'></td><td><input type=text value='please enter engine size of car'></td><td><input type=button value='commit change'></tr>")
-      });
-     </script>
-   </table>
+       ?>
+       <script>
+        $(document).ready(function() {
+          $("table").append("<tr><td><input type=text value='please enter make of car'></td><td><input type=text value='please enter model of car'></td><td><input type=text value='please enter engine size of car'></td><td><input type=button value='commit change'></tr>")
+        });
+       </script>
+     </table>
+    </div>
   </body>
 </html>
 
